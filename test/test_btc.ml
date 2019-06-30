75926d252a9f1486e5b8061d9eafc6870ac834a5
Vincent Bernardoff <vb@luminar.eu.org>
2019-06-30 12:49:32 +0200

    btc: upgrade test

--- test/test_btc.ml
+++ test/test_btc.ml
@@ -2,25 +2,25 @@ open Ledgerwallet_btc
 open Bitcoin.Protocol
 open Bitcoin.Util
 
-let ctx = Secp256k1.Context.create []
+module Secp256k1 = Libsecp256k1.External
+
+let ctx = Secp256k1.Context.create ()
 let prevTx = Transaction.of_hex (`Hex "0100000001c3798bf6520ac4e95e24c587b6ee25a1c492f33ddd35615f90f38d89e8e2b47c010000006b483045022100dc48cef9d3e1eb71e84bcf51ceaf7f938328573e482bf8af951e9b53e87a74c802206280177d6ac07455d9984a8dd62f8d9f87c91884820c9fa587c8ada46750a44d4121032af552f85308e3c68c9751c415a5efe01fc165a955e48835a84894ab9986b149ffffffff02005ed0b2000000001976a914c78d002920f40f471846083f4283eae42246035988acb0cecff5150000001976a9147e854f6a0d4b20f61ba91ab0aa8e1f6f428e628e88ac00000000")
 let nextTx2 = Transaction.of_hex (`Hex "01000000018f514dcffb6242ff6c2b01aa888700dce343f57e265e72c9fb46607065ffad48000000001976a914b2483f8a51043dca4144748f91e45e83e473730d88acffffffff01e0567b4d000000005d14cf244fd81c2d7cee628190f75b48ca621cbf3fcc75522102e8f164c9c12b039d3e522aa291e3544ddc99240a878080669aee1c359f66d2532103c6ad7f526553affb3979483dac278b1517199a5ac3cd0fbb669644aadbf933ba52ae00000000")
 let path = Bitcoin.Wallet.KeyPath.[to_hardened 44l; to_hardened 1l; to_hardened 0l; 0l; 0l]
 
 let nextTx =
-  let open Bitcoin in
-  let open Bitcoin.Util in
   let open Bitcoin.Protocol in
   (* Format.printf "%a@." Transaction.pp prevTx ; *)
   let prev_out_hash = Transaction.hash256 prevTx in
-  let my_out = List.hd prevTx.outputs in
+  let my_out = prevTx.outputs.(0) in
   let input =
     TxIn.create' ~prev_out_hash ~prev_out_i:0 ~script:my_out.script () in
   let value = Int64.sub my_out.value 100000L in
   (* Printf.printf "%Ld %Ld\n%!" my_out.value value ; *)
   let output =
     TxOut.create ~value ~script:my_out.script in
-  Transaction.create ~inputs:[input] ~outputs:[output] ()
+  Transaction.create ~inputs:[|input|] ~outputs:[|output|] ()
 
 let test_open_close () =
   let h = Hidapi.open_id_exn ~vendor_id:0x2C97 ~product_id:0x0001 in
@@ -83,9 +83,9 @@ let test_sign_segwit () =
                     O (Op_pushdata (Cstruct.len pk_compressed)) ;
                     D pk_compressed ;
                    ] in
-  let nextTx_input = List.hd nextTx.inputs in
+  let nextTx_input = nextTx.inputs.(0) in
   let nextTx_input = { nextTx_input with script = scriptSig @ nextTx_input.script } in
-  let txFinal = { nextTx with inputs = [nextTx_input] } in
+  let txFinal = { nextTx with inputs = [|nextTx_input|] } in
   let cs = Cstruct.create 1024 in
   let cs' = Bitcoin.Protocol.Transaction.to_cstruct cs txFinal in
   let txSerialized = Cstruct.sub cs 0 cs'.off in
@@ -93,12 +93,13 @@ let test_sign_segwit () =
   Hidapi.close h
 
 let basic = [
-  "open_close", `Quick, test_open_close ;
-  "ping", `Quick, test_ping ;
-  "get_info", `Quick, test_get_info ;
-  "get_random", `Quick, test_get_random ;
-  "get_wallet_pk", `Quick, test_get_wallet_pk ;
-  "get_trusted_input", `Quick, test_get_trusted_input ;
+  (* "open_close", `Quick, test_open_close ;
+   * "ping", `Quick, test_ping ; *)
+
+  (* "get_info", `Quick, test_get_info ;
+   * "get_random", `Quick, test_get_random ;
+   * "get_wallet_pk", `Quick, test_get_wallet_pk ;
+   * "get_trusted_input", `Quick, test_get_trusted_input ; *)
   "sign_segwit", `Quick, test_sign_segwit ;
 ]
 
